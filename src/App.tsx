5eff98a016bbe9b8a4a4b2c319394a39c33c9721
Simon Mollet <mollet.simon.pro@gmail.com>
2023-05-10 19:39:10 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Content">
+      <p>test</p>
+      <h1 className="title">test titre</h1>
+        <button className="button">bouton</button>
+      <p>test content</p>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import {Admin} from "./pages/Admin";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import {CreateTournament} from "./pages/CreateTournament";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAuoJd0tqoF23elawzcpaDU02-Mu9azKlw",
+    authDomain: "church-tournament.firebaseapp.com",
+    projectId: "church-tournament",
+    storageBucket: "church-tournament.appspot.com",
+    messagingSenderId: "154721190099",
+    appId: "1:154721190099:web:f00f921dc824cbd4bf1426",
+    measurementId: "G-087KHJ547J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Routes>
+              <Route path="/" element={<App/>}></Route>
+              <Route path="/admin" element={<Admin/>}/>
+              <Route path="/create-tournament" element={<CreateTournament/>} />
+          </Routes>
+      </Router>
   </React.StrictMode>
 );
 
