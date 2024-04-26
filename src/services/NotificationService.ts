67cb131250589d35d90461fbb49befb9fb32a076
MolletSimon <mollet.simon.pro@gmail.com>
2024-04-27 01:30:26 +0200

    update notif url

--- src/services/NotificationService.ts
+++ src/services/NotificationService.ts
@@ -1,15 +1,15 @@
-const url = "http://localhost:3001"
+const url = "https://api-ongoal.onrender.com";
 
-export const  NotifyUser = (message: string, title: string, team: string) => {
-    fetch(`${url}/notification`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            message,
-            title,
-            team
-        })
-    })
+export const NotifyUser = (message: string, title: string, team: string) => {
+  fetch(`${url}/notification`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message,
+      title,
+      team,
+    }),
+  });
 };
